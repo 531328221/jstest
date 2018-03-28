aa07c9c11574f934ebe82d106b91a2dc89bc32ae
yang <531328221@qq.com>
2018-03-28 17:03:40 +0800

    all modules

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            0,
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console":[
+            1
+        ]
+    }
+};
